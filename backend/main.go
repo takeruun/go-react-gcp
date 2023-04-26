eb7ca01ca4033265e8454b669c6b92b1d6425101
takeru.hashimoto <hstake25@gmail.com>
2023-04-26 22:06:07 +0900

    キャッシュ

--- backend/main.go
+++ backend/main.go
@@ -8,8 +8,18 @@ func main() {
 }
 
 func SetRouting() {
+	http.Handle("/", setCacheControl(http.FileServer(http.Dir("dist"))))
+
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"message": "ok"}`))
+	})
+}
+
+// cache 設定
+func setCacheControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=86400")
+		h.ServeHTTP(w, r)
 	})
 }
